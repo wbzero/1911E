63a4998b664502d06c61856d267b3725b3e21f21
李庆波 <li276366837@163.com>
2020-09-29 16:26:42 +0800

    添加

--- src/router/index.js
+++ src/router/index.js
@@ -52,6 +52,110 @@ const routes = [
     meta:{
       show:true
     }
+  },
+  {
+    path: '/currency',
+    name: 'Currency',
+    component: () => import('../views/user/Currency.vue'),
+    meta:{
+      show:false
+    }
+  },
+  {
+    path: '/friends',
+    name: 'Friends',
+    component: () => import('../views/user/Friends.vue'),
+    meta:{
+      show:false
+    }
+  },
+  {
+    path: '/star',
+    name: 'Star',
+    component: () => import('../views/user/Star.vue'),
+    meta:{
+      show:false
+    }
+  },
+  {
+    path: '/bill',
+    name: 'Bill',
+    component: () => import('../views/user/Bill.vue'),
+    meta:{
+      show:false
+    }
+  },
+  {
+    path: '/bills',
+    name: 'Bills',
+    component: () => import('../views/user/Bills.vue'),
+    meta:{
+      show:false
+    }
+  },
+  {
+    path: '/billse',
+    name: 'Billse',
+    component: () => import('../views/user/Billse.vue'),
+    meta:{
+      show:false
+    }
+  },
+  {
+    path: '/coupon',
+    name: 'Coupon',
+    component: () => import('../views/user/Coupon.vue'),
+    meta:{
+      show:false
+    }
+  },
+  {
+    path: '/card',
+    name: 'Card',
+    component: () => import('../views/user/Card.vue'),
+    meta:{
+      show:false
+    }
+  },
+  {
+    path: '/diamond',
+    name: 'Diamond',
+    component: () => import('../views/user/Diamond.vue'),
+    meta:{
+      show:false
+    }
+  },
+  {
+    path: '/bell',
+    name: 'Bell',
+    component: () => import('../views/user/Bell.vue'),
+    meta:{
+      show:false
+    }
+  },
+  {
+    path: '/invition',
+    name: 'Invition',
+    component: () => import('../views/user/Invition.vue'),
+    meta:{
+      show:false
+    }
+  },
+  {
+    path: '/service',
+    name: 'Service',
+    component: () => import('../views/user/Service.vue'),
+    meta:{
+      show:false
+    }
+  },
+  {
+    path: '/setting',
+    name: 'Setting',
+    component: () => import('../views/user/Setting.vue'),
+    meta:{
+      show:false
+    }
   }
 ]
 
---
+++ src/views/user/Bills.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="box">
+      <my-title
+      :title="title"
+      />
+      <div>
+        <van-tabs v-model="active">
+          <van-tab title="全部订单">
+            <van-empty
+            class="custom-image"
+            image="https://img.yzcdn.cn/vant/custom-empty-image.png"
+            description="暂无订单"
+          />
+          </van-tab>
+          <van-tab title="待支付">
+            <van-empty
+            class="custom-image"
+            image="https://img.yzcdn.cn/vant/custom-empty-image.png"
+            description="暂无订单"
+          />
+          </van-tab>
+          <van-tab title="已完成">
+            <van-empty
+            class="custom-image"
+            image="https://img.yzcdn.cn/vant/custom-empty-image.png"
+            description="暂无订单"
+          />
+          </van-tab>
+          <van-tab title="已取消">
+            <van-empty
+            class="custom-image"
+            image="https://img.yzcdn.cn/vant/custom-empty-image.png"
+            description="暂无订单"
+          />
+          </van-tab>
+        </van-tabs>
+        
+      </div>
+  </div>
+</template>
+
+<script>
+import title from "./components/Title"
+export default {
+  data() {
+    return {
+      title:"会员订单",
+      details:true,
+      active: 1,
+    }
+  },
+  components:{
+    "my-title":title
+  }
+
+}
+</script>
+
+<style scoped>
+.box{
+  width: 100%;
+  height: 100vh;
+  background-color: #F0F2F5;
+}
+ .custom-image .van-empty__image {
+    width: 90px;
+    height: 90px;
+  }
+</style>
