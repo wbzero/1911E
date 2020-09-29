72b5f95ef127bb154ac732b8125215eb89b90884
Luke.Deng <7982804+yjd11@user.noreply.gitee.com>
2020-09-29 08:43:02 +0800

    tj

--- src/router/index.js
+++ src/router/index.js
@@ -7,14 +7,14 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'Home', // 主页面
     component: Home,
     meta:{
       show:true
     }
   },
   {
-    path: '/login',
+    path: '/login', // 登录页面
     name: 'Login',
     component: () => import('../views/Login.vue'),
     meta:{
@@ -22,7 +22,7 @@ const routes = [
     }
   },
   {
-    path: '/lessone',
+    path: '/lessone',  // 课程页面
     name: 'Lessone',
     component: () => import('../views/lessone/index.vue'),
     meta:{
@@ -30,7 +30,7 @@ const routes = [
     }
   },
   {
-    path: '/course',
+    path: '/course',  // 约课记录页面
     name: 'Course',
     component: () => import('../views/course/index.vue'),
     meta:{
@@ -38,7 +38,15 @@ const routes = [
     }
   },
   {
-    path: '/practice',
+    path: '/coach', // 约课记录里的一对一辅导
+    name: 'coach',
+    component: () => import('../views/course/Coach.vue'),
+    meta:{
+      show:false
+    }
+  },
+  {
+    path: '/practice', // 练习页面
     name: 'Practice',
     component: () => import('../views/practice/index.vue'),
     meta:{
@@ -46,13 +54,14 @@ const routes = [
     }
   },
   {
-    path: '/user',
+    path: '/user', // 我的页面
     name: 'User',
     component: () => import('../views/user/index.vue'),
     meta:{
       show:true
     }
-  }
+  },
+  
 ]
 
 const router = new VueRouter({
