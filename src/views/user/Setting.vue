907d79b9fcbe94da1aeb24d75b9fd925af0cb1d1
wbzero <1348257825@qq.com>
2020-10-04 04:56:03 +0800

    改

--- src/views/user/Setting.vue
+++ src/views/user/Setting.vue
@@ -1,15 +1,42 @@
 <template>
-  <div>
-      Setting
+  <div class="setting_wrap">
+      <div class="setting_top">
+        <div>
+          <van-icon name="arrow-left" @click="back" size="22" />
+        </div>
+        <div>
+          设置
+        </div>
+        <div></div>
+      </div>
+      <div>
+        
+      </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    back(){
+      this.$router.go(-1)
+    }
+  },
 }
 </script>
 
-<style>
-
+<style scoped>
+.setting_wrap{
+  width: 100%;
+  height: 100vh;
+  background-color: #EFF1F4;
+}
+.setting_top{
+  width: 100%;
+  background-color: #fff;
+  font-size: .18rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 </style>
